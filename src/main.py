567577e1793daca8ffdb6e244763262c4d47fd20
Kyle <ky42@protonmail.com>
2022-09-13 22:47:24 -0230

    Acidentaly uploaded token fixed and formated main.py better

--- src/main.py
+++ src/main.py
@@ -1,19 +1,29 @@
 import discord
 from discord.ext import commands
-
-MY_GUILD = discord.Object(id=378743556526964737) 
+import os
+from dotenv import load_dotenv
 
 class Bot(commands.Bot):
     async def setup_hook(self: commands.Bot):
+        # Loads all cogs
         await self.load_extension("cogs.game")
-        # await self.load_extension("cogs.tournament")
-        # await self.load_extension("cogs.money")
+        await self.load_extension("cogs.tournament")
+        await self.load_extension("cogs.money")
+
+        # Syncs commands to mals server
+        MY_GUILD = discord.Object(id=378743556526964737) 
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
 #TODO update to only needed intents
-
 intents = discord.Intents.default()
 bot = Bot(command_prefix="/", intents=intents)
 
-bot.run('MTAwNzgyOTY3MDQyNDc1NjI5NQ.G5n_0Z.xvS5biMQ5eUkoCHuBGkn10hRe-9J5ghts1Ugss')
+# Gets token from env and runs bot with it
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+if token:
+    bot.run(token)
+else:
+    print("Please set token in .env")
+
