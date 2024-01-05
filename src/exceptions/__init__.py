608dc058efbce89eb0961708d7aa48116ae51968
Kyle <ky42@protonmail.com>
2024-01-05 12:31:33 -0330

    Documented and cleaned exceptions folder

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,10 +1,20 @@
-from data_types import GameId
+from data_types import GameId, UserId
 
 
 class UserNotFound(Exception):
-    """Raised when looking for a user but they are not found"""
+    """Raised when user can not be found.
 
-    def __init__(self, user_id: int, *args: object) -> None:
+    Attributes:
+        user_id: user id that was not found.
+    """
+
+    def __init__(self, user_id: UserId, *args: object) -> None:
+        """Initializes the exception with the user id.
+
+        Args:
+            user_id (UserId): Id of the user that was not found.
+        """
+
         self.user_id = user_id
         super().__init__(*args)
 
@@ -13,11 +23,21 @@ class UserNotFound(Exception):
 
 
 class GameNotFound(Exception):
-    """Raised when game is not found"""
+    """Raised when game is not found.
 
-    def __init__(self, game_name: GameId, *args: object) -> None:
-        self.game_name = game_name
+    Attributes:
+        game_id: game id that was not found.
+    """
+
+    def __init__(self, game_id: GameId, *args: object) -> None:
+        """Initializes the exception with the game id.
+
+        Args:
+            game_id (GameId): Id of the game that was not found.
+        """
+
+        self.game_id = game_id
         super().__init__(*args)
 
     def __str__(self) -> str:
-        return f"Game {self.game_name} not found"
+        return f"Game with id {self.game_id} not found"
