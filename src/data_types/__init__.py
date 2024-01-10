13aa8854a85c8b601efeb2bf5d55dd38c886e7be
Kyle <ky42@protonmail.com>
2024-01-10 16:20:27 -0330

    Documented data type folder

--- src/data_types/__init__.py
+++ src/data_types/__init__.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import discord
 
+# Type aliases
 GameId = str
 UserId = int
 MessageId = int
@@ -10,12 +11,28 @@ MessageId = int
 
 @dataclass
 class DiscordMessage:
+    """Stores data for a message to be sent to Discord.
+
+    Attributes:
+        content (str): Content of message.
+        ephemeral (bool): Whether message is only visible to user its sent to.
+        view (Optional[discord.ui.View]): View to be attached to message.
+        embed (Optional[discord.Embed]): Embed to be attached to message.
+    """
+
     content: str
     ephemeral: bool = True
     view: Optional[discord.ui.View] = None
     embed: Optional[discord.Embed] = None
 
-    def for_send(self):
+    def for_send(self) -> dict:
+        """Creates a dictionary of attributes for sending to Discord.
+
+        Removes any attributes with a value of None.
+
+        Returns:
+            dict: Dictionary of attributes.
+        """
         return {
             item: value for item, value in self.__dict__.items() if value is not None
         }
--- src/data_types/protocols.py
+++ src/data_types/protocols.py
@@ -2,4 +2,9 @@ from typing import ClassVar, Dict, Protocol
 
 
 class IsDataclass(Protocol):
+    """Protocol for checking if a class is a dataclass.
+
+    Used to make sure paramters passed to functions are dataclasses.
+    """
+
     __dataclass_fields__: ClassVar[Dict]
